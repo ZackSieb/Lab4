42abef05f846045ed27394e951bfcfd9c9eededa
Siebu <Siebu@DESKTOP-UH0H628>
2019-10-14 00:57:54 -0500

    Car

--- src/main/java/CircleTest.java
+++ src/main/java/CircleTest.java
@@ -18,5 +18,13 @@ public class CircleTest {
         
         userInputString = JOptionPane.showInputDialog( "Please enter the circle's radius: ");
         userRadius = Double.parseDouble( userInputString);
+        
+        CircleClass circle1 = new CircleClass();
+        circle1.setRadius( userRadius);
+        String userOutputString = String.format("Area: %.2f\nDiameter: %.2f\nCircumference: %.2f", circle1.getArea(), circle1.getDiameter(), circle1.getCircumference() );
+        
+        JOptionPane.showMessageDialog( null, userOutputString);
+        System.exit(0);
+        
     }
 }
